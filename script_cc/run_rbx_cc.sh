397bbc071cf6e7196564ee8be7605e8bc7ba3101
PaulBautin <pbautin70@gmail.com>
2022-06-21 18:01:30 -0400

    update run_combine_flows_cc.txt with inputs and outputs for compute canada
    update run_rbx_cc.sh module load and position of atlas directory for compute canada

--- script_cc/run_rbx_cc.sh
+++ script_cc/run_rbx_cc.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # This would run RecobundleX with the following parameters:
-#   - Dti_shells 0 and 300, Fodf_shells 0 and 2000.
-#   - WM seeding
+#   - Population average atlas for RecobundlesX DOI: 10.5281/zenodo.5165374
 
 
 #SBATCH --nodes=1              # --> Generally depends on your nb of subjects.
@@ -23,9 +22,12 @@
 #SBATCH --mail-type=ALL
 
 
+module load StdEnv/2020 java/14.0.2 nextflow/22.04.3 singularity/3.8
+
+
 my_singularity_img='/home/pabaua/scratch/scil_dev/containers/scilus_1.3.0.sif' # or .img
 my_main_nf='/home/pabaua/scratch/scil_dev/rbx_flow/main.nf'
-my_input='/home/pabaua/scratch/tpil_dev/data/dataset_sansan_rbx'
+my_input='/home/pabaua/scratch/tpil_dev/data/dataset_CON_rbx'
 my_atlas_config='/home/pabaua/scratch/scil_dev/atlas/config.json'
 my_atlas_anat='/home/pabaua/scratch/scil_dev/atlas/mni_masked.nii.gz'
 my_atlas_dir='/home/pabaua/scratch/scil_dev/atlas/atlas'
@@ -35,4 +37,5 @@ my_atlas_centroids='/home/pabaua/scratch/scil_dev/atlas/centroids'
 
 NXF_DEFAULT_DSL=1 nextflow run $my_main_nf --input $my_input \
     -with-singularity $my_singularity_img -resume -with-report report.html \
-    --atlas_config $my_atlas_config --atlas_anat $my_atlas_anat --atlas_directory $my_atlas_dir --atlas_centroids $my_atlas_centroids
+    --atlas_config $my_atlas_config --atlas_anat $my_atlas_anat \
+    --atlas_directory $my_atlas_dir --atlas_centroids $my_atlas_centroids
