595641ff2b3bb4974d6bae00a40f28ef1ee1f300
PaulBautin <pbautin70@gmail.com>
2022-11-03 15:35:23 -0400

    tree for bundle mosaic

---
+++ bundle_segmentation/bundle_mosaic/run_tree_for_bundle_mosaic.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+module singularity/3.8
+
+my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.3.0.sif' # or .img
+
+bundles=/home/pabaua/scratch/tpil_dev/results/clbp/22-10-26_bundle_seg/results_bundle/
+tracto=/home/pabaua/scratch/tpil_dev/results/clbp/22-09-23_tractoflow_bundling/results/
+output=/home/pabaua/scratch/tpil_dev/results/clbp/22-11-03_bundle_qc/
+
+command='bash tree_for_bundle_mosaic.sh -b $bundles -t $tracto -o $output'
+
+singularity exec $my_singularity_img $command
+
+
+
+
+
+
