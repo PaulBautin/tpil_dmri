124aa220a5110ce7bec6ff5be472e8df0a7cbfaf
PaulBautin <pbautin70@gmail.com>
2022-11-03 16:04:51 -0400

    tree for bundle mosaic

--- bundle_segmentation/bundle_mosaic/tree_for_bundle_mosaic.sh
+++ bundle_segmentation/bundle_mosaic/tree_for_bundle_mosaic.sh
@@ -36,7 +36,7 @@ do
     # Output
     output=$o/results/$i/$i.png
 
-    singularity exec $my_singularity_img scil_visualize_bundles_mosaic.py $ref_image $ref_image $output
+    singularity exec -B /home -B /project -B /scratch -B /localscratch $my_singularity_img scil_visualize_bundles_mosaic.py $ref_image $ref_image $output
 done
 echo "Done"
 
