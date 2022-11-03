2931355a09d2824b18682966a554be845d3c71bc
PaulBautin <pbautin70@gmail.com>
2022-11-03 15:42:21 -0400

    tree for bundle mosaic

--- bundle_segmentation/bundle_mosaic/run_tree_for_bundle_mosaic.sh
+++ bundle_segmentation/bundle_mosaic/run_tree_for_bundle_mosaic.sh
@@ -4,15 +4,13 @@ module load singularity/3.8
 
 my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.3.0.sif' # or .img
 
-bundles=/home/pabaua/scratch/tpil_dev/results/clbp/22-10-26_bundle_seg/results_bundle/
-tracto=/home/pabaua/scratch/tpil_dev/results/clbp/22-09-23_tractoflow_bundling/results/
-output=/home/pabaua/scratch/tpil_dev/results/clbp/22-11-03_bundle_qc/
+bundles='/home/pabaua/scratch/tpil_dev/results/clbp/22-10-26_bundle_seg/results_bundle/'
+tracto='/home/pabaua/scratch/tpil_dev/results/clbp/22-09-23_tractoflow_bundling/results/'
+output='/home/pabaua/scratch/tpil_dev/results/clbp/22-11-03_bundle_qc/'
 
-run=/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_dmri/bundle_segmentation/bundle_mosaic/tree_for_bundle_mosaic.sh
+run='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_dmri/bundle_segmentation/bundle_mosaic/tree_for_bundle_mosaic.sh'
 
-command='bash $run -b $bundles -t $tracto -o $output'
-
-singularity exec $my_singularity_img $command
+singularity exec $my_singularity_img bash $run -b $bundles -t $tracto -o $output
 
 
 
