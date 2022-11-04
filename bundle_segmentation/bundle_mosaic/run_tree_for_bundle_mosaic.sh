083712af9d91ad40505d61ffc2811c5cf1e9d4b9
PaulBautin <pbautin70@gmail.com>
2022-11-04 09:20:26 -0400

    run new bundle

--- bundle_segmentation/bundle_mosaic/run_tree_for_bundle_mosaic.sh
+++ bundle_segmentation/bundle_mosaic/run_tree_for_bundle_mosaic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bundles='/home/pabaua/scratch/tpil_dev/results/clbp/22-10-26_bundle_seg/results_bundle'
+bundles='/home/pabaua/scratch/tpil_dev/results/clbp/22-11-03_bundle_seg/results_bundle'
 tracto='/home/pabaua/scratch/tpil_dev/results/clbp/22-09-23_tractoflow_bundling/results'
 output='/home/pabaua/scratch/tpil_dev/results/clbp/22-11-03_bundle_qc'
 run='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_dmri/bundle_segmentation/bundle_mosaic/tree_for_bundle_mosaic.sh'
