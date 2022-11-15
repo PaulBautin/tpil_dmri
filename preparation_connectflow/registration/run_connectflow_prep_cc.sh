177a9be66c860d4260e139b8a31852eb1021850e
PaulBautin <pbautin70@gmail.com>
2022-11-14 23:19:16 -0500

    run freesurfer_cc

--- preparation_connectflow/registration/run_connectflow_prep_cc.sh
+++ preparation_connectflow/registration/run_connectflow_prep_cc.sh
@@ -29,7 +29,7 @@ module load StdEnv/2020 java/14.0.2 nextflow/22.04.3 singularity/3.8
 
 my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.3.0.sif' # or .sif
 my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_dmri/preparation_connectflow/registration/main.nf'
-my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-14_connectflow_prep_clbp/'
+my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-14_connectflow_prep_control/'
 my_atlas='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-14_connectflow_prep_clbp/BNA-maxprob-thr0-1mm.nii.gz'
 my_template='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-14_connectflow_prep_clbp/FSL_HCP1065_FA_1mm.nii.gz'
 
