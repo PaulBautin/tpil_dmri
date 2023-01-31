c386df4c2e194266f852322d90cba4e8262b0483
PaulBautin <pbautin70@gmail.com>
2023-01-31 13:24:23 -0500

    decrease number of points for tractometry

--- tractometry/run_tractometry_cc.sh
+++ tractometry/run_tractometry_cc.sh
@@ -26,7 +26,7 @@ module load StdEnv/2020 java/14.0.2 nextflow/22.04.3 singularity/3.8
 
 my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.4.2.sif' # or .sif
 my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/tpil_dmri/tractometry/main.nf'
-my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-25_tractometry_clbp/'
+my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-11-25_tractometry_control/'
 
 
 
