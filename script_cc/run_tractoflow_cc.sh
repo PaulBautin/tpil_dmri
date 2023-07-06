2b4b80b87a20487ad4b85826367eabfcf764deb3
PaulBautin <pbautin70@gmail.com>
2023-07-06 07:30:07 -0400

    new scripts

--- script_cc/run_tractoflow_cc.sh
+++ script_cc/run_tractoflow_cc.sh
@@ -30,9 +30,9 @@ module load StdEnv/2020 java/14.0.2 nextflow/22.10.8 apptainer/1.1.8
 
 git -C /home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/tractoflow checkout tags/2.4.2
 
-my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.5.0.sif' # or .img
+my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.4.2.sif' # or .img
 my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/tractoflow/main.nf'
-my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-08-19_dMRI_control_BIDS/'
+my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/22-08-19_dMRI_CLBP_BIDS/'
 
 nextflow run $my_main_nf --bids $my_input \
     -with-singularity $my_singularity_img -resume -with-report report.html \
