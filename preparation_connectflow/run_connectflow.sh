6f85f83cf81550499a4f3ee839c1f6f9422fb346
PaulBautin <pbautin70@gmail.com>
2023-07-07 11:25:25 -0400

    new scripts

--- preparation_connectflow/run_connectflow.sh
+++ preparation_connectflow/run_connectflow.sh
@@ -2,13 +2,13 @@
 
 
 
-my_singularity_img='/home/pabaua/dev_scil/containers/scilus_1_3_0.img' # or .img
+my_singularity_img='/home/pabaua/dev_scil/containers/containers_scilus_1.5.0.sif' # or .img
 my_main_nf='/home/pabaua/dev_scil/connectoflow/main.nf'
-my_input='/home/pabaua/dev_tpil/data/connectflow_data'
-label_list='/home/pabaua/dev_tpil/data/connectflow_data/atlas_brainnetome_v4_labels_list.txt'
-template='/home/pabaua/Downloads/fsl-6.0.5.2-sources/fsl/data/standard/MNI152_T1_1mm_brain.nii.gz'
+my_input='/home/pabaua/dev_tpil/results/results_connectivity_prep/test_container/results'
+label_list='/home/pabaua/dev_tpil/tpil_network_analysis/connectivity_prep/freesurfer_data/atlas_brainnetome_first_label_list.txt'
+template='/home/pabaua/dev_tpil/tpil_network_analysis/connectivity_prep/freesurfer_data/mni_masked.nii.gz'
 
 
-NXF_DEFAULT_DSL=1 nextflow run $my_main_nf --input $my_input --labels_list $label_list --template $template \
-    -with-singularity $my_singularity_img -resume
+NXF_DEFAULT_DSL=1 NXF_VER=21.10.6 nextflow run $my_main_nf --input $my_input --labels_list $label_list --template $template \
+    --apply_t1_labels_transfo false --nbr_subjects_for_avg_connections 2 -with-singularity $my_singularity_img -resume
 
