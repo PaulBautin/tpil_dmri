c28f430abc35378565f8f5bcaf532e4ab2cfe19c
PaulBautin <pbautin70@gmail.com>
2023-09-12 11:20:05 -0400

    update run rbx

--- script_local/run_rbx_local.sh
+++ script_local/run_rbx_local.sh
@@ -4,14 +4,15 @@
 #   - Dti_shells 0 and 300, Fodf_shells 0 and 2000.
 #   - WM seeding
 
+git -C /home/pabaua/dev_scil/rbx_flow checkout 1.2.0
 
 my_singularity_img='/home/pabaua/dev_scil/containers/containers_scilus_1.5.0.sif' # or .sif
 my_main_nf='/home/pabaua/dev_scil/rbx_flow/main.nf'
-my_input='/home/pabaua/dev_tpil/data/Pascal'
-my_atlas_config='/home/pabaua/dev_scil/atlas/config/config_ind.json'
-my_atlas_anat='/home/pabaua/dev_scil/atlas/atlas_2023/mni_masked.nii.gz'
-my_atlas_dir='/home/pabaua/dev_scil/atlas/atlas_2023/atlas'
-my_atlas_centroids='/home/pabaua/dev_scil/atlas/atlas_2023/centroids'
+my_input='/home/pabaua/dev_tpil/results/23-09-12_rbx_test'
+my_atlas_config='/home/pabaua/dev_scil/atlas_v22/config/config_ind.json'
+my_atlas_anat='/home/pabaua/dev_scil/atlas_v22/atlas/mni_masked.nii.gz'
+my_atlas_dir='/home/pabaua/dev_scil/atlas_v22/atlas/atlas'
+my_atlas_centroids='/home/pabaua/dev_scil/atlas_v22/atlas/centroids'
 
 
 
